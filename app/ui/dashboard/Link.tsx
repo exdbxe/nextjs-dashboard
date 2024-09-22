f43b9ef0ab28209fa14becf26969fa1d71f35c79
itzyexd <ed.syed69@gmail.com>
2024-09-23 02:48:51 +0800

    Updated. No error for now.

---
+++ app/ui/dashboard/Link.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { IconType } from "../../../../../../../AppData/Local/deno/npm/registry.npmjs.org/react-icons/5.3.0/lib/iconBase";
+
+export interface Link {
+  name: string;
+  href: string;
+  icon: IconType;
+}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+ 
 import {
   UserGroupIcon,
   HomeIcon,
@@ -8,16 +8,12 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+
+// Define the links array
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Home', href: '/', icon: HomeIcon },
+  { name: 'Users', href: '/users', icon: UserGroupIcon },
+  { name: 'Documents', href: '/documents', icon: DocumentDuplicateIcon },
 ];
 
 export default function NavLinks() {
